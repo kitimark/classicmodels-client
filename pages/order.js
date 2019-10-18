95e427e1e0e9690aacf9c032f19e7fe164adf5ab
ekawitjaidee <ekawit_jaidee@cmu.ac.th>
2019-10-18 16:43:34 +0700

    Initialized order

---
+++ pages/order.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+
+
+const order = () => (
+
+
+<table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+  <thead>
+    <tr style={{backgroundColor:'lightgreen'}}>
+      <th><abbr title="Product Code">Product Code</abbr></th>
+      <th><abbr title="Customer">Customer</abbr></th>
+      <th><abbr title="Date">Date</abbr></th>
+      <th><abbr title="Status">Status</abbr></th>
+  
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>2</th>
+      <th>3</th>
+      <th>4</th>
+    </tr>
+    </tbody>
+  </table>
+
+)
+
+
+export default order

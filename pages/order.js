70f435e629e8385bee95b0e8e74f2a6055aa2358
ekawitjaidee <ekawit_jaidee@cmu.ac.th>
2019-12-11 16:25:32 +0700

    Update Order page

--- pages/order.js
+++ pages/order.js
@@ -5,7 +5,6 @@ import { useQuery } from 'react-apollo'
 
 
 const GET_ORDERS = gql`
-
 query{
   orders{
     _id
@@ -16,49 +15,62 @@ query{
     shippedDate
   }
 }
-
 `
 
 
 
-const order = () => {
+const OrderTable = () =>{
   const { loading, error, data } = useQuery(GET_ORDERS)
 
   if (loading) return <p>Loading...</p>
+
+  return(
+    <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+    <thead>
+      <tr style={{ backgroundColor: 'lightgreen' }}>
+        <th><abbr title="orderNumber">OrderNumber</abbr></th>
+        <th><abbr title="orderDate">OrderDate</abbr></th>
+        <th><abbr title="requiedDate">RequiredDate</abbr></th>
+        <th><abbr title="shippedDate">ShippedDate</abbr></th>
+        <th><abbr title="Status">Status</abbr></th>
+
+      </tr>
+    </thead>
+
+    {data.orders.map(order => {
+      return(
+        <tbody>
+        <tr>
+          <td>{order.orderNumber}</td>
+          <td>{order.orderDate}</td>
+          <td>{order.requiredDate}</td>
+          <td>{order.shippedDate}</td>
+          <td>{order.status}</td>
+        </tr>
+      </tbody>
+        )
+    })}
+
+
+  </table>
+  )
+    
+
+
+  
+
+}
+
+
+const order = () => {
+  
 
 
   return (
 
     <Prototype title="ORDER">
 
-      <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-        <thead>
-          <tr style={{ backgroundColor: 'lightgreen' }}>
-            <th><abbr title="orderNumber">OrderNumber</abbr></th>
-            <th><abbr title="orderDate">OrderDate</abbr></th>
-            <th><abbr title="requiedDate">RequiredDate</abbr></th>
-            <th><abbr title="shippedDate">ShippedDate</abbr></th>
-            <th><abbr title="Status">Status</abbr></th>
-
-          </tr>
-        </thead>
-
-        {data.orders.map(order => {
-          return(
-            <tbody>
-            <tr>
-              <td>{order.orderNumber}</td>
-              <td>{order.orderDate}</td>
-              <td>{order.requiredDate}</td>
-              <td>{order.shippedDate}</td>
-              <td>{order.status}</td>
-            </tr>
-          </tbody>
-            )
-        })}
-
-
-      </table>
+     <OrderTable/>
 
     </Prototype>
   )

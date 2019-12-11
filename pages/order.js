45a172eb25f4efca15177bebde3cff17b495495a
ekawitjaidee <ekawit_jaidee@cmu.ac.th>
2019-12-11 15:07:36 +0700

    Rendor Order page

--- pages/order.js
+++ pages/order.js
@@ -1,71 +1,68 @@
 import React from 'react'
 import Prototype from '../components/prototype'
+import gql from 'graphql-tag'
+import { useQuery } from 'react-apollo'
+
+
+const GET_ORDERS = gql`
+
+query{
+  orders{
+    _id
+    orderNumber
+   	status 
+    orderDate
+    requiredDate
+    shippedDate
+  }
+}
+
+`
 
 
 
-const order = () => (
-<Prototype title="ORDER">
+const order = () => {
+  const { loading, error, data } = useQuery(GET_ORDERS)
 
-<table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-  <thead>
-    <tr style={{backgroundColor:'lightgreen'}}>
-      <th><abbr title="Product Code">Product Code</abbr></th>
-      <th><abbr title="Customer">Customer</abbr></th>
-      <th><abbr title="Date">Date</abbr></th>
-      <th><abbr title="Status">Status</abbr></th>
-  
-    </tr>
-  </thead>
+  if (loading) return <p>Loading...</p>
 
-  <tbody>
-    <tr>
-      <th>1</th>
-      <th>2</th>
-      <th>3</th>
-      <th>4</th>
-    </tr>
-    <tr>
-      <th>1</th>
-      <th>2</th>
-      <th>3</th>
-      <th>4</th>
-    </tr>
-    <tr>
-      <th>1</th>
-      <th>2</th>
-      <th>3</th>
-      <th>4</th>
-    </tr>
-    <tr>
-      <th>1</th>
-      <th>2</th>
-      <th>3</th>
-      <th>4</th>
-    </tr>
-    <tr>
-      <th>1</th>
-      <th>2</th>
-      <th>3</th>
-      <th>4</th>
-    </tr>
-    <tr>
-      <th>1</th>
-      <th>2</th>
-      <th>3</th>
-      <th>4</th>
-    </tr>
-    <tr>
-      <th>1</th>
-      <th>2</th>
-      <th>3</th>
-      <th>4</th>
-    </tr>
-    </tbody>
-  </table>
 
-</Prototype>
+  return (
 
-)
+    <Prototype title="ORDER">
+
+      <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr style={{ backgroundColor: 'lightgreen' }}>
+            <th><abbr title="orderNumber">OrderNumber</abbr></th>
+            <th><abbr title="orderDate">OrderDate</abbr></th>
+            <th><abbr title="requiedDate">RequiredDate</abbr></th>
+            <th><abbr title="shippedDate">ShippedDate</abbr></th>
+            <th><abbr title="Status">Status</abbr></th>
+
+          </tr>
+        </thead>
+
+        {data.orders.map(order => {
+          return(
+            <tbody>
+            <tr>
+              <td>{order.orderNumber}</td>
+              <td>{order.orderDate}</td>
+              <td>{order.requiredDate}</td>
+              <td>{order.shippedDate}</td>
+              <td>{order.status}</td>
+            </tr>
+          </tbody>
+            )
+        })}
+
+
+      </table>
+
+    </Prototype>
+  )
+}
 
 
 export default order

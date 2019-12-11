222f9659288c93063de123feae7e2d946ef186ec
ekawitjaidee <ekawit_jaidee@cmu.ac.th>
2019-12-11 23:10:31 +0700

    add customer

--- pages/member/create.js
+++ pages/member/create.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Prototype from '../../components/prototype'
+import { gql } from 'apollo-boost'
+import {  Mutation } from "react-apollo"
 
 
-const Form = () => (
-  <>
+const CREATE_CUSTOMER = gql`
+  mutation createCustomer($input: CustomerInput!){
+    createCustomer(input: $input){
+      _id
+    }
+  }
+`
+
+const Form = () => {
+
+  const [Firstname, setfirstname] = useState('')
+  const [Lastname, setlastname] = useState('')
+  const [Company, setcompany] = useState('')
+  const [Phone, setphone] = useState('')
+  const [Addressline1, setaddressline1] = useState('')
+  const [Addressline2, setaddressline2] = useState('')
+  const [City, setcity] = useState('')
+  const [State, setstate] = useState('')
+  const [Postalcode, setpostalcode] = useState('')
+  const [Country, setcountry] = useState('')
+
+  // console.log(firstname)
+  return(
+
+    <>
     <div class="field is-horizontal">
       <div class="field-label is-normal">
         <label class="label">NAME</label>
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="Firstname" />
+          <input onChange={event => setfirstname(event.target.value)} class="input" type="text" placeholder="Firstname" />
         </div>
         <div class="field">
-          <input class="input" type="text" placeholder="Lastname" />
+          <input onChange={event => setlastname(event.target.value)} class="input" type="text" placeholder="Lastname"  />
         </div>
       </div>
     </div>
@@ -24,7 +49,7 @@ const Form = () => (
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="Company Name" />
+          <input onChange={event => setcompany(event.target.value)} class="input" type="text" placeholder="Company Name" />
         </div>
       </div>
     </div>
@@ -35,18 +60,29 @@ const Form = () => (
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="Phone Number" />
+          <input onChange={event => setphone(event.target.value)} class="input" type="text" placeholder="Phone Number" />
         </div>
       </div>
     </div>
 
     <div class="field is-horizontal">
       <div class="field-label is-normal">
-        <label class="label">ADDRESS</label>
+        <label class="label">ADDRESSLINE1</label>
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="Your address" />
+          <input onChange={event => setaddressline1(event.target.value)} class="input" type="text" placeholder="Your address" />
+        </div>
+      </div>
+    </div>
+
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">ADDRESSLINE2</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <input onChange={event => setaddressline2(event.target.value)} class="input" type="text" placeholder="Your address" />
         </div>
       </div>
     </div>
@@ -57,7 +93,7 @@ const Form = () => (
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="..." />
+          <input onChange={event => setcity(event.target.value)} class="input" type="text" placeholder="..." />
         </div>
       </div>
     </div>
@@ -68,18 +104,18 @@ const Form = () => (
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="..." />
+          <input onChange={event => setstate(event.target.value)} class="input" type="text" placeholder="..." />
         </div>
       </div>
     </div>
 
     <div class="field is-horizontal">
       <div class="field-label is-normal">
-        <label class="label">Portal Code</label>
+        <label class="label">Postal Code</label>
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="..." />
+          <input onChange={event => setpostalcode(event.target.value)} class="input" type="text" placeholder="..." />
         </div>
       </div>
     </div>
@@ -90,26 +126,79 @@ const Form = () => (
       </div>
       <div class="field-body">
         <div class="field">
-          <input class="input" type="text" placeholder="..." />
+          <input onChange={event => setcountry(event.target.value)} class="input" type="text" placeholder="..." />
         </div>
       </div>
     </div>
 
-    <div class="field is-grouped is-grouped-centered">
-      <p class="control">
-        <a class="button is-primary">CREATE</a>
+  <Mutation mutation={CREATE_CUSTOMER}>
+    {(createCustomer, { data, loading, error }) => (
+      <div>
+        <form onClick={e =>{
+          console.log({ 
+            firstName: Firstname, 
+            lastName: Lastname, 
+            company: Company,
+            phone: Phone,
+            addresses: [
+              {
+                addressLine1: Addressline1,
+                addressLine2: Addressline2,
+                city: City,
+                state: State,
+                postalCode: Postalcode,
+                country: Country
+              }
+            ]})
+          e.preventDefault();
+          createCustomer({variables: {input: { 
+            firstName: Firstname, 
+            lastName: Lastname, 
+            company: Company,
+            phone: Phone,
+            addresses: [
+              {
+                addressLine1: Addressline1,
+                addressLine2: Addressline2,
+                city: City,
+                state: State,
+                postalCode: Postalcode,
+                country: Country
+              }
+            ]
+          }}})
+        }}
+>
+<div className="field is-grouped is-grouped-centered">
+      <p className="control">
+        <a className="button is-primary" type="submit">CREATE</a>
+        {loading &&
+      <div>adding person…</div>
+      }
+      { data &&
+      <div>response data</div>
+      }
+      { error &&
+      <div>Error adding person…</div>
+      }
       </p>
-      <p class="control">
-        <a class="button is-light">CANCEL</a>
+      <p className="control">
+        <a className="button is-light">CANCEL</a>
       </p>
     </div>
-
-     
+</form>
+      </div>
+    )
+    }
+    
+    
+    </Mutation>   
  
 
   </>
 
 )
+}
 
 const CreateMember = () => (
   <Prototype title="CREATE MEMBER">

a4457b0aad7bae2c738d91bc804a5696271f5712
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-12-12 10:52:23 +0700

    Update fetch username

--- components/prototype/Navuser.js
+++ components/prototype/Navuser.js
@@ -1,30 +1,47 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserTie } from '@fortawesome/free-solid-svg-icons'
+import { useQuery } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-const Level = ({ title }) => (
-    <div>
-        <nav className="level">
-            {/* left */}
-            <div className="level-left">
-                <div className="level-item">
-                    <h1 class="title has-text-light">{title}</h1>
-                </div>
-            </div>
+const GET_ME = gql`
+{
+    me {
+        firstName
+    }
+}
+`
 
-            {/* right */}
-            <div>
-                <div className="level-right">
-                    <p className="level-item">
-                        <h1 class="title has-text-light">user  </h1>
-                    </p>
-                    <div style={{color: "#383839", paddingTop:10 , paddingRight:10}}>
-                        <FontAwesomeIcon icon={faUserTie}  size="2x"/>
+const Level = ({ title }) =>  {
+    const {loading, error, data} = useQuery(GET_ME)
+
+    // if (loading) return <p>loading</p>
+    // if (error) return <p>{error.message}</p>
+
+    return (
+        <div>
+            <nav className="level">
+                {/* left */}
+                <div className="level-left">
+                    <div className="level-item">
+                        <h1 class="title has-text-light">{title}</h1>
                     </div>
                 </div>
-            </div>
-        </nav>
-    </div>
-)
+
+                {/* right */}
+                <div>
+                    <div className="level-right">
+                        <p className="level-item">
+                            <h1 class="title has-text-white">{(!loading) ? error.message || data.me.firstName : 'loading'}</h1>
+                        </p>
+                        <div style={{color: "#383839", paddingTop:10 , paddingRight:10}}>
+                            <FontAwesomeIcon icon={faUserTie}  size="2x"/>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
 
 export default Level
